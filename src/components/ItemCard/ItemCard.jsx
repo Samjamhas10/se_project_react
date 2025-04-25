5b68e12391acce85827d73c532a1fbe3f346afc0
SamSam Hashi <sammymadobe@gmail.com>
2025-04-25 18:40:13 -0500

    Finish implementing ItemCard functionality

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,10 +1,19 @@
 import "./ItemCard.css";
 
-function ItemCard({ item }) {
+function ItemCard({ item, onCardClick }) {
+  const handleCardClick = () => {
+    onCardClick(item);
+  };
+
   return (
     <li className="card">
       <h1 className="card__name">{item.name}</h1>
-      <img className="card__image" src={item.link} alt={item.name} />
+      <img
+        onClick={handleCardClick}
+        className="card__image"
+        src={item.link}
+        alt={item.name}
+      />
     </li>
   );
 }
