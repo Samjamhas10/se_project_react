0234ff61efb2f7f4514732b1a5dd1b81f3d2242b
SamSam Hashi <sammymadobe@gmail.com>
2025-04-25 16:55:51 -0500

    Implemnt opening and closing the modalwithform

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,14 @@ import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
 
-function Header() {
+function Header({ handleAddClick }) {
   return (
     <header className="header">
-      <img className="header__logo" src={logo}/>
+      <img className="header__logo" src={logo} />
       <p className="header__date-and-location">June 15, New York</p>
-      <button className="header__add-clothes-btn">+ Add clothes</button>
+      <button onClick={handleAddClick} className="header__add-clothes-btn">
+        + Add clothes
+      </button>
       <div className="header__user-container">
         <p className="header__username">Terrence Tegegne</p>
         <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
