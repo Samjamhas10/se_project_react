3c3f05094ab36e9696e3417830d806a121327ec1
SamSam Hashi <sammymadobe@gmail.com>
2025-04-25 20:34:47 -0500

    Add more styles to ItemModal.css

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -2,17 +2,21 @@ import "./ItemModal.css";
 
 function ItemModal({ activeModal, onClose, card }) {
   return (
-    <div className={`modal ${activeModal === "preview" && "modal__opened"}`}>
-      <div className="modal__content modal__content_type_image">
+    <div
+      className={`weather-modal ${
+        activeModal === "preview" && "weather-modal__opened"
+      }`}
+    >
+      <div className="weather-modal__content weather-modal__content_type_image">
         <button
           onClick={onClose}
           type="button"
           className="modal__close"
         ></button>
-        <img src={card.link} alt="" className="modal__image" />
-        <div className="modal__footer">
-          <h2 className="modal__caption">{card.name}</h2>
-          <p className="modal__weather">Weather: {card.weather}</p>
+        <img src={card.link} alt="" className="weather-modal__image" />
+        <div className="weather-modal__footer">
+          <h2 className="weather-modal__caption">{card.name}</h2>
+          <p className="weather-modal__weather">Weather: {card.weather}</p>
         </div>
       </div>
     </div>
